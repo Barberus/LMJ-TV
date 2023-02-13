1428152f3ad942996bd9c22ef56cf5c3a6ce20d2
Barberus <markusdittrich456@gmail.com>
2023-02-13 12:26:19 +0100

    Delete backup directory

--- backup/202302120629/addons/plugin.picture.googlephotos/README.md
+++
@@ -1,8 +0,0 @@
-# Google Photos Kodi Addon
-View Google Photos on Kodi!
-* Unlimited accounts
-* Shared Album Support
-* Custom Filter Support
-* Video Playback (Exists but broken due to Photos API limitations)
-
-This program is not affiliated with or sponsored by Google.
--- backup/202302120629/addons/plugin.video.rtlgroup.de/service.py
+++
@@ -1,8 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-
-import xbmcaddon
-
-if __name__ == '__main__':
-    xbmcaddon.Addon().setSetting('service_startWINDOW', 'true')
-    xbmcaddon.Addon().setSetting('service_startWIDEVINE', 'true')
-    xbmcaddon.Addon().setSetting('maximum_tries', '0')
